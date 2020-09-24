27946baa33d9f1a4557424114bf58891fa3b82ea
Simon Tidman <simontidman@Simons-MacBook-Pro.local>
2020-09-24 16:39:40 +0100

    removed bs from settings

--- screenpop/settings.py
+++ screenpop/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'django_forms_bootstrap',
-    'bootstrap4',
+
     'crispy_forms',
     'password_reset',
     'business',
