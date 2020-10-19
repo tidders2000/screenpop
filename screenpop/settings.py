3466ee3d4327fc0cb43981ae7e06cb32338e5648
Simon Tidman <simontidman@Simons-MacBook-Pro.local>
2020-10-19 19:15:00 +0100

    fixed blog menu - appleicon - login text

--- screenpop/settings.py
+++ screenpop/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-#import env
+import env
 import dj_database_url
 from machina import MACHINA_MAIN_TEMPLATE_DIR
 from machina import MACHINA_MAIN_STATIC_DIR
