78faeeced4429800b073b77b37603642e22bf841
Simon Tidman <simontidman@Simons-MBP.lan>
2021-02-25 16:26:34 +0000

    changed meeting template

--- uploader/urls.py
+++ uploader/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('', profile_upload, name="profile_upload"),
     path('meeting_upload', meeting_upload, name="meeting_upload"),
+    path('switcher_upload', switcher_upload, name="switcher_upload"),
+    path('business_upload', business_upload, name="business_upload"),
 
 ]
--- uploader/views.py
+++ uploader/views.py
@@ -96,3 +96,72 @@ def profile_upload(request):
         )
     context = {}
     return render(request, template, context)
+
+
+@login_required
+def switcher_upload(request):
+    # declaring template
+    template = "switcher_upload.html"
+    data = User.objects.all()
+# prompt is a context variable that can have different values      depending on their context
+    prompt = {
+        'order': 'Order of the CSV should be name, email, address,    phone, profile',
+        'profiles': data
+    }
+    # GET request returns the value of the data with the specified key.
+    if request.method == "GET":
+        return render(request, template, prompt)
+    csv_file = request.FILES['file']
+    # let's check if it is a csv file
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'THIS IS NOT A CSV FILE')
+    data_set = csv_file.read().decode('UTF-8')
+
+    # setup a stream which is when we loop through each line we are able to handle a data in a stream
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        _, created = Switcher.objects.update_or_create(
+            user=User.objects.filter(username=column[0]).first(),
+            group=Groups.objects.filter(pk=column[1]).first(),
+
+            business_profile=BusinessProfile.objects.filter(
+                pk=column[2]).first()
+
+        )
+
+    context = {}
+    return render(request, template, context)
+
+
+@login_required
+def business_upload(request):
+    # declaring template
+    template = "business_upload.html"
+    data = User.objects.all()
+# prompt is a context variable that can have different values      depending on their context
+    prompt = {
+        'order': 'Order of the CSV should be name, email, address,    phone, profile',
+        'profiles': data
+    }
+    # GET request returns the value of the data with the specified key.
+    if request.method == "GET":
+        return render(request, template, prompt)
+    csv_file = request.FILES['file']
+    # let's check if it is a csv file
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'THIS IS NOT A CSV FILE')
+    data_set = csv_file.read().decode('UTF-8')
+
+    # setup a stream which is when we loop through each line we are able to handle a data in a stream
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        _, created = BusinessProfile.objects.update_or_create(
+            user=User.objects.filter(username=column[0]).first(),
+            business_name=column[1]
+
+        )
+
+    context = {}
+    return render(request, template, context)
